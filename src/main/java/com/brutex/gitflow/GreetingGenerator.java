782cd19d82b98d7f136d31dfa3c887cae9dff77e
jurgen.goris <jurgen.goris@brutex.com>
2026-09-25 15:43:29 +0200

    WHS-003: refactor to GreetingGenerator

--- src/main/java/com/brutex/gitflow/GreetingGenerator.java
+++ src/main/java/com/brutex/gitflow/GreetingGenerator.java
@@ -2,7 +2,7 @@ package com.brutex.gitflow;
 
 import java.util.Optional;
 
-public class MessageGenerator
+public class GreetingGenerator
 {
     public static String build(String name){
         return "Hello, " + Optional.ofNullable(name).orElse("unknown") + "!";
